a5258c029bc1c5c777013e173a11dc3e4306aeaa
Muhammad Bilal <116734226+Bilal202202001@users.noreply.github.com>
2024-05-06 14:22:48 +0500

    Update TrackAll.jsx

--- client/src/MarketPlace/TrackOrders/components/TrackAll.jsx
+++ client/src/MarketPlace/TrackOrders/components/TrackAll.jsx
@@ -6,6 +6,7 @@ function TrackAll({ post }) {
     const [sessionUser, setSessionuser] = useState('');
 
     useEffect(() => {
+        axios.defaults.withCredentials = true;
         axios.get('https://mern-marketplace-dapp.vercel.app/auth/getMail')
             .then(res => {
                 const userEmail = res.data.email;
