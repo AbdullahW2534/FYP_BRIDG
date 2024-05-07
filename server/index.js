4563396d309216b97cacf72f62bcede6d5df8762
Bilal202202001 <muhammad.bilal.94390@gmail.com>
2024-05-07 09:39:10 +0500

    Sever

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-  origin: 'https://mern-marketplace-dapp-frontend.vercel.app',
+  origin: 'https://solestyle.vercel.app',
   methods: "GET,POST,PUT,DELETE, PATCH",
   credentials: true
 
