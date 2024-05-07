2fc6391ce13a7b824398e7035cae005d7aa219e4
Bilal202202001 <muhammad.bilal.94390@gmail.com>
2024-05-07 09:57:46 +0500

    Cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(cors({
   origin: 'https://solestyle.vercel.app',
-  methods: "GET,POST,PUT,DELETE, PATCH",
+  methods: ['GET','POST','PUT','DELETE', 'PATCH'],
   credentials: true
 
 }));
