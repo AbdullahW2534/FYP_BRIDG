554fae49c9ca638de02497dee6df8c4f4f069e76
Bilal202202001 <muhammad.bilal.94390@gmail.com>
2024-05-08 15:23:47 +0500

    Animations

--- client/src/MarketPlace/Blogs/components/BlogsPalet.jsx
+++ client/src/MarketPlace/Blogs/components/BlogsPalet.jsx
@@ -12,22 +12,25 @@ export default function BlogsPalet({ postsData }) {
         WebkitBoxOrient: 'vertical',
         overflow: 'hidden',
         textOverflow: 'ellipsis',
-        transition: 'transform 0.3s ease', 
-        transform: isClicked ? 'translateY(10px)' : 'translateY(0)', 
+        transition: 'transform 0.3s ease',
+        transform: isClicked ? 'translateY(10px)' : 'translateY(0)',
     };
 
     return (
-        <div className='p-5 '>
+        <div className=''
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-center"
+        >
             <img src={`${postsData.image}`} className='w-full h-72 lg:h-[30rem]  lg:p-4' alt={`img`} />
             <h2 className='w-full text-center font-medium mb-1'>{postsData.heading}</h2>
-            <h2 className='w-full flex justify-end items-center mt-1 text-gray-400 px-4  text-xs'><Unicons.UilPostStamp  width={16} className="text-yellow-500 ml-3 mr-1" />{postsData.auther}</h2>
+            <h2 className='w-full flex justify-end items-center mt-1 text-gray-400 px-4  text-xs'><Unicons.UilPostStamp width={16} className="text-yellow-500 ml-3 mr-1" />{postsData.auther}</h2>
             <h2 className="w-full  mt-1" style={ellipsisStyle}>
                 {postsData.content}
             </h2>
             <div className='w-full flex justify-end'>
-            <Unicons.UilEye onClick={handleClick} width={20} className="mt-4 text-red-600 cursor-pointer" />
+                <Unicons.UilEye onClick={handleClick} width={20} className="mt-4 text-red-600 cursor-pointer" />
             </div>
-            
+
         </div>
     );
 }
