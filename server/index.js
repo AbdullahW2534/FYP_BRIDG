e881e25607d9b86526a2cebe264a8c5557ee7727
Abdullah Waseem <107957394+AbdullahW2534@users.noreply.github.com>
2024-07-05 22:01:41 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import axios from 'axios';
 const app = express();
 app.use(express.json())
 app.use(cors({
-  origin: ['https://bridg-ruby.vercel.app', 'http://localhost:5173','https://api.paystack.co/refund','https://api.paystack.co/balance'],
+  origin: ['https://bridg-aws.vercel.app', 'http://localhost:5173','https://api.paystack.co/refund','https://api.paystack.co/balance'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
